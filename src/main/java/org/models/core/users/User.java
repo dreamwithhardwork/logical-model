17008cd3f391d3f4bad379ece1f2a1a5f0c4d2c0
srikanth <srikanth2121098@gmail.com>
2020-10-22 14:46:16 +0530

    making sure user is unique

--- src/main/java/org/models/core/users/User.java
+++ src/main/java/org/models/core/users/User.java
@@ -4,13 +4,16 @@ package org.models.core.users;
 import lombok.Getter;
 import lombok.Setter;
 import org.models.core.validators.UserValidator;
+import org.springframework.data.mongodb.core.index.Indexed;
 
 @Getter
 @Setter
 @UserValidator
 public class User {
     private String name;
+    @Indexed(sparse = true, unique = true)
     private String mobile;
+    @Indexed(sparse = true, unique = true)
     private String email;
     private String password;
 }
