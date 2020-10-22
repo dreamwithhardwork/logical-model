a0cd11f164cba5c1822c40248d80e18672118b7f
srikanth <srikanth2121098@gmail.com>
2020-10-23 01:24:01 +0530

    added id for supporting update

--- src/main/java/org/models/core/users/User.java
+++ src/main/java/org/models/core/users/User.java
@@ -6,12 +6,16 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 import org.models.core.validators.UserValidator;
+import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.index.Indexed;
 
 @Getter
 @Setter
 @UserValidator
 public class User {
+
+    @Id
+    private String id;
     private String name;
     @Indexed(sparse = true, unique = true)
     private String mobile;
