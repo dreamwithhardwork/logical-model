9b866339def896a1d7bb09bb2530d7a9d2a162bc
srikanth bathi <srikanth.b@turvo.com>
2022-01-15 18:52:20 +0530

    libs

--- src/main/java/org/models/core/dao/BookingRepository.java
+++ src/main/java/org/models/core/dao/BookingRepository.java
@@ -8,7 +8,4 @@ import java.util.List;
 
 @Repository
 public interface BookingRepository extends MongoRepository<Booking, String> {
-
-
-
 }
--- src/main/java/org/models/core/enums/BookingStatus.java
+++ src/main/java/org/models/core/enums/BookingStatus.java
@@ -1,4 +1,5 @@
 package org.models.core.enums;
 
 public enum BookingStatus {
+    TESTRIDE, BOOKING
 }
